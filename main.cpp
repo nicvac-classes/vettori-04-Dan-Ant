d1cfaebef350c5a7a89c8ca1072cdc7fb0a41099
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2025-02-12 15:58:10 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,85 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int n, p, i, pes, pos, max, min;
+cout << "Quanti elementi devono essere originati?" << endl;
+cin >> n;
+int v[n];
+i=0;
+while (i<n)
+{
+    v[i]= rand() % 1000;
+    i=i+1;
+
+}
+i=0;
+p=0;
+while (i<n)
+{
+   if( v[i]%2=0)
+   {
+    p=p+1;
+
+   }
+   i=i+1;
+}
+cout << "Sono stati generati " << p << " numeri pari" << endl;
+i=0;
+while (i<n)
+{
+    if(i=0)
+    {
+        min = v[i];
+
+    }
+    else
+    {
+        if(min>v[i])
+        {
+            min=v[i];
+        }
+    }
+    i=i+1;
+}
+i=0;
+while (i<n)
+{
+   if(min=v[i])
+   {
+    pos=i;
+   }
+   i=i+1;
+}
+cout << "Il valore più piccolo è " << min << " nella posizione " << pos << endl;
+i=0;
+while (i<n)
+{
+    if(i=0)
+    {
+        max = v[i];
+
+    }
+    else
+    {
+        if(max<v[i])
+        {
+            max=v[i];
+        }
+    }
+    i=i+1;
+}
+i=0;
+while (i<n)
+{
+   if(max=v[i])
+   {
+    pes=i;
+   }
+   i=i+1;
+}
+cout << "Il valore più grande è " << max << " nella posizione " << pes << endl;
+
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
